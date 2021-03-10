01d519e885f9af729df952ba00c8eb95b45be1a0
Will Jones <wij21@bath.ac.uk>
2021-03-10 18:08:37 +0000

    Changed the users table to 'User_Accounts'

--- database/database_sqlite.sql
+++ database/database_sqlite.sql
@@ -1,5 +1,5 @@
 --
--- File generated with SQLiteStudio v3.3.2 on Wed Mar 10 18:01:15 2021
+-- File generated with SQLiteStudio v3.3.2 on Wed Mar 10 18:07:45 2021
 --
 -- Text encoding used: System
 --
@@ -7,13 +7,13 @@ PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
 -- Table: Exercise_Sessions
-CREATE TABLE Exercise_Sessions (exercise_session_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, user_id INTEGER NOT NULL REFERENCES User_Account (user_id), exercise_name VARCHAR NOT NULL, exercise_time DOUBLE NOT NULL);
+CREATE TABLE Exercise_Sessions (exercise_session_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, user_id INTEGER NOT NULL REFERENCES User_Accounts (user_id), exercise_name VARCHAR NOT NULL, exercise_time DOUBLE NOT NULL);
 
 -- Table: Study_Sessions
-CREATE TABLE Study_Sessions (study_session_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, user_id INTEGER NOT NULL REFERENCES User_Account (user_id), study_name VARCHAR NOT NULL, study_time DOUBLE NOT NULL);
+CREATE TABLE Study_Sessions (study_session_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, user_id INTEGER NOT NULL REFERENCES User_Accounts (user_id), study_name VARCHAR NOT NULL, study_time DOUBLE NOT NULL);
 
--- Table: User_Account
-CREATE TABLE User_Account (user_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR NOT NULL, password VARCHAR NOT NULL, forename VARCHAR NOT NULL, surname VARCHAR NOT NULL, email VARCHAR NOT NULL);
+-- Table: User_Accounts
+CREATE TABLE User_Accounts (user_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR NOT NULL, password VARCHAR NOT NULL, forename VARCHAR NOT NULL, surname VARCHAR NOT NULL, email VARCHAR NOT NULL);
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
