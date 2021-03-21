4eebe7aac88028bf7a02c312f9801ab5cf84b430
GarraCharrua <78643133+GarraCharrua@users.noreply.github.com>
2021-03-21 18:04:39 +0000

    Temp timer design

---
+++ src/main/gui/executingTimer/executingTimer.java
@@ -0,0 +1,10 @@
+package main.gui.executingTimer;
+
+import javax.swing.*;
+
+public class executingTimer {
+    private JLabel minRemaining;
+    private JLabel secRemaining;
+    private JButton stopButton;
+    private JButton playPauseButton;
+}
---
+++ src/main/gui/studyTimer/timerSetup.java
@@ -0,0 +1,11 @@
+package main.gui.studyTimer;
+
+import javax.swing.*;
+
+public class timerSetup {
+    private JLabel workLabel;
+    private JLabel restLabel;
+    private JTextField workInput;
+    private JTextField restInput;
+    private JButton playButton;
+}
