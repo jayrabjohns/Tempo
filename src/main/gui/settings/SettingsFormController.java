53f06a269acab6d5a532b63ee5cbdaf3a2ceca71
Archey Barrell <archey.barrell@outlook.com>
2021-04-04 23:37:03 +0100

    Added logout success alert

--- src/main/gui/settings/SettingsFormController.java
+++ src/main/gui/settings/SettingsFormController.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import main.gui.AbstractMainFormController;
+import main.gui.JAlert;
 import main.gui.Screen;
 
 public class SettingsFormController extends AbstractMainFormController {
@@ -11,6 +12,7 @@ public class SettingsFormController extends AbstractMainFormController {
     public void bindLogoutButton(JButton logoutButton) {
         logoutButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                Screen.getForm("login").showAlert(new JAlert(JAlert.TYPE_SUCCESS, "Success!", "You have successfully been logged out."));
                 Screen.showForm("login");
             }
         });
