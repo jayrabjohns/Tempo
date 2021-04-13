318cd95aeec423e4ce748c882c6ccbcd3c01cea0
Wisaacj <wisaacj@live.co.uk>
2021-04-13 21:43:38 +0100

    Preparing analysis backend

--- src/main/backend/analysis/HistoryAnalysis.java
+++ src/main/backend/analysis/HistoryAnalysis.java
@@ -1,7 +1,68 @@
 package main.backend.analysis;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public class HistoryAnalysis {
 
-    
+    private Connection conn;
+
+    public HistoryAnalysis() {
+
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+
+            this.conn = DriverManager.getConnection("jdbc:mysql://pyp.wwlrc.co.uk/group3?user=group3&password=bathuni");
+
+            PreparedStatement s = conn.prepareStatement("SELECT * FROM User_Accounts");
+
+            ResultSet result = s.executeQuery();
+            result.next();
+            System.out.println(result.getString("username"));
+
+
+        } catch(Exception er) {
+
+            System.err.println(er.getMessage());
+
+        }
+
+    }
+
+    public ArrayList<String> getWeekSummary() {
+
+        ArrayList<String> output = new ArrayList<>();
+
+        return output;
+
+    }
+
+    public ArrayList<String> getMonthSummary() {
+
+        ArrayList<String> output = new ArrayList<>();
+
+        return output;
+
+    }
+
+    public ArrayList<String> getYearSummary() {
+
+        ArrayList<String> output = new ArrayList<>();
+
+        return output;
+
+    }
+
+    public ArrayList<String> getOverallSummary() {
+
+        ArrayList<String> output = new ArrayList<>();
+
+        return output;
+
+    }
 
 }
