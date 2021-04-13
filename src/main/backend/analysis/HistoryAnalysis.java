5196e7dc7f4c27abee56bb88c4c79f073535150c
Wisaacj <wisaacj@live.co.uk>
2021-04-13 22:36:38 +0100

    Initial data analysis
    Creating the basic data analysis functions

--- src/main/backend/analysis/HistoryAnalysis.java
+++ src/main/backend/analysis/HistoryAnalysis.java
@@ -1,9 +1,6 @@
 package main.backend.analysis;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class HistoryAnalysis {
@@ -18,11 +15,15 @@ public class HistoryAnalysis {
 
             this.conn = DriverManager.getConnection("jdbc:mysql://pyp.wwlrc.co.uk/group3?user=group3&password=bathuni");
 
-            PreparedStatement s = conn.prepareStatement("SELECT * FROM User_Accounts");
+            // PreparedStatement s = conn.prepareStatement("SELECT * FROM Study_Sessions WHERE user_id = 1");
 
-            ResultSet result = s.executeQuery();
-            result.next();
-            System.out.println(result.getString("username"));
+            // ResultSet result = s.executeQuery();
+
+            // while (result.next()) {
+
+                // System.out.println(result.getDouble("study_time"));
+
+            // }
 
 
         } catch(Exception er) {
@@ -33,7 +34,7 @@ public class HistoryAnalysis {
 
     }
 
-    public ArrayList<String> getWeekSummary() {
+    public ArrayList<String> getWeekSummary(String username) {
 
         ArrayList<String> output = new ArrayList<>();
 
@@ -57,11 +58,71 @@ public class HistoryAnalysis {
 
     }
 
-    public ArrayList<String> getOverallSummary() {
+    public ArrayList<String> getOverallSummary(String username) {
 
         ArrayList<String> output = new ArrayList<>();
 
+        double averageStudyTime = 0;
+        double averageExerciseTime = 0;
+
+        try {
+
+            PreparedStatement studyStatement = conn.prepareStatement("SELECT study_time FROM Study_Sessions WHERE user_id = (SELECT user_id FROM User_Accounts WHERE username = " + username + ")");
+            ResultSet studyResults = studyStatement.executeQuery();
+            averageStudyTime = this.getAverageTime(studyResults,"study_time");
+
+            PreparedStatement exerciseStatement = conn.prepareStatement("SELECT exercise_time FROM Exercise_Sessions WHERE user_id = (SELECT user_id FROM User_Accounts WHERE username = " + username + ")");
+            ResultSet exerciseResults = exerciseStatement.executeQuery();
+            averageExerciseTime = this.getAverageTime(exerciseResults,"exercise_time");
+
+        } catch (Exception er) {
+
+            System.err.println(er.getMessage());
+
+        }
+
+        output.add(Double.toString(averageStudyTime));
+        output.add(Double.toString(averageExerciseTime));
+
         return output;
+
+    }
+
+    private double getAverageTime(ResultSet results, String sessionType) {
+
+        double output = 0;
+        double lenResults = 0;
+
+        try {
+
+            while (results.next()) {
+
+                lenResults += 1;
+                output += results.getDouble(sessionType);
+
+            }
+
+        } catch (Exception er) {
+
+            System.err.println(er.getMessage());
+
+        }
+
+        // Finding the average
+        output /= lenResults;
+
+        return output;
+
+    }
+
+    public static void main(String args[]) {
+
+        HistoryAnalysis ha = new HistoryAnalysis();
+
+        ArrayList<String> output = ha.getOverallSummary("'archey.barrell'");
+        System.out.println(output.get(0));
+        System.out.println(output.get(1));
+
 
     }
 
