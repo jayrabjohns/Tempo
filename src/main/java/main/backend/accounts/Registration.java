78267305dfb21e9f6c2b8748204b59a9dc3901bd
Archey Barrell <archey.barrell@outlook.com>
2021-04-25 19:35:42 +0100

    Added checks for username/email on registration

--- src/main/java/main/backend/accounts/Registration.java
+++ src/main/java/main/backend/accounts/Registration.java
@@ -3,8 +3,19 @@ package main.backend.accounts;
 import main.backend.validation.Validator;
 import main.backend.validation.InvalidUserInputException;
 import main.backend.DBHandler;
+import main.backend.PasswordEncryption;
 
-public class Registration {
+public class Registration { 
+
+    private DBHandler db;
+    private PasswordEncryption hasher;
+
+    public Registration(DBHandler db, PasswordEncryption hasher) {
+        this.db = db;
+        this.hasher = hasher;
+    }
+
+
     public void register(String username, String password, String retypePassword, String forename, String surname, String email) throws InvalidUserInputException {
 
         Validator.require(forename, "Please enter in your forename");
@@ -21,11 +32,26 @@ public class Registration {
         Validator.requireEquality(password, retypePassword, "Your passwords do not match");
         
         // Check a user with that username doesn't exist
+        String[][] users = db.retrieveUserInfo(0);
+
+        for(String[] user: users) {
+            if (user[0] == null) {
+                break;
+            }
+
+            if(user[0].equals(username)) {
+                throw new InvalidUserInputException("A user with that username already exists");
+            }
+
+            if(user[2].equals(email)) {
+                throw new InvalidUserInputException("A user with that email already exists");
+            }
+        }
 
         // Encrypt password
-        password = Authenticator.encryptPassword(password);
+        password = hasher.encryptPassword(password);
 
         // Otherwise insert it
-        DBHandler.insertNewUser(username, password, forename, surname, email);
+        db.insertNewUser(username, password, forename, surname, email);
     }
 }
